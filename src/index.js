34fe3417402260675ec52f56a8117e95cc77c8ec
Wiktor Karaszewicz <wkaraszewicz@gmail.com>
2021-12-13 22:10:47 +0100

    Adding faker and creating a comment with faker

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 // Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 
 // Create a react component
-const App = () => {
+const Hello_world = () => {
     return (
         <div>
             <label class="label" for='name'>
@@ -11,9 +12,36 @@ const App = () => {
             </label>
             <input id='name' type='text' />
             <button style={{ backgroundColor: "blue", color: "white"}}> Submit </button>
+            <br></br>
+            <h2>Hi There</h2>
         </div>
+        
+        
     )
 };
 
+const Comments = () => {
+    return (
+        <div className="ui container comments">
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" src={faker.image.avatar()} />
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Sam
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 6:00PM</span>
+                    </div>
+                    <div className="text">This was very good</div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
 // Take the react component and show it on the screen
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<Hello_world />, document.querySelector('#root'));
+ReactDOM.render(<Comments />, document.querySelector('#comments'));
+
